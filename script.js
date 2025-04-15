1659cd9fb8029898a48fc9cd40c36c9b1e80a304
Mariam <mariebukhaidze@gmail.com>
2025-04-15 20:31:49 +0400

    Corrected

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function logIn() {
   let logInContainer = document.querySelector(".logIn-container");
+  let registrationContainer = document.querySelector(
+    ".registration-container"
+  );
 
   let isHidden =
     logInContainer.style.display === "none" ||
@@ -7,13 +10,17 @@ function logIn() {
 
   if (isHidden) {
     logInContainer.style.display = "block";
+    registrationContainer.style.display = "none";
   } else {
     logInContainer.style.display = "none";
   }
 }
 
 function registrationForm() {
-  let registrationContainer = document.querySelector(".registration-container");
+  let registrationContainer = document.querySelector(
+    ".registration-container"
+  );
+  let logInContainer = document.querySelector(".logIn-container");
 
   let isHidden =
     registrationContainer.style.display === "none" ||
@@ -21,7 +28,33 @@ function registrationForm() {
 
   if (isHidden) {
     registrationContainer.style.display = "block";
+    logInContainer.style.display = "none";
   } else {
+    registrationContainer.style.display = "none";
+  }
+}
+
+// functional for body 
+function hideForms(e) {
+  let logInContainer = document.querySelector(".logIn-container");
+  let registrationContainer = document.querySelector(
+    ".registration-container"
+  );
+
+  let isInsideLogIn = logInContainer.contains(e.target);
+  let isInsideRegistration = registrationContainer.contains(e.target);
+  let isLogInButton = e.target.closest("button[onclick='logIn()']");
+  let isRegistrationButton = e.target.closest(
+    "button[onclick='registrationForm()']"
+  );
+
+  if (
+    !isInsideLogIn &&
+    !isInsideRegistration &&
+    !isLogInButton &&
+    !isRegistrationButton
+  ) {
+    logInContainer.style.display = "none";
     registrationContainer.style.display = "none";
   }
 }
