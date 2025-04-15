f54958cc54bba7b4ff996188b88302ce10f83298
Mariam <mariebukhaidze@gmail.com>
2025-04-15 13:51:41 +0400

    Need Correct

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function logIn() {
   let logInContainer = document.querySelector(".logIn-container");
 
-  let isHidden = 
-  logInContainer.style.display === "none" ||
-  logInContainer.style.display === "";
-
+  let isHidden =
+    logInContainer.style.display === "none" ||
+    logInContainer.style.display === "";
 
   if (isHidden) {
     logInContainer.style.display = "block";
-  
   } else {
     logInContainer.style.display = "none";
+  }
+}
+
+function registrationForm() {
+  let registrationContainer = document.querySelector(".registration-container");
+
+  let isHidden =
+    registrationContainer.style.display === "none" ||
+    registrationContainer.style.display === "";
+
+  if (isHidden) {
+    registrationContainer.style.display = "block";
+  } else {
+    registrationContainer.style.display = "none";
   }
 }
