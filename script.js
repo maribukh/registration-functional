687d3498ec061751b1175291ec0970936263a593
maribukh <mariebukhaidze@gmail.com>
2025-04-29 13:19:40 +0400

    Corrected Visibility Eye Functional

--- script.js
+++ script.js
@@ -61,30 +61,22 @@ function hideForms(e) {
 }
 
 function togglePasswordVisibility(event) {
-  let eyeNoneVisibility = document.querySelector(".eyeNoneVisibility");
-  let eyeVisibility = document.querySelector(".eyeVisibility");
+  let eyebox = event.currentTarget;
+  let container = eyebox.closest(".password-container");
+  if (!container) return;
 
-  let container = event
-    ? event.target.closest(".password-container")
-    : document.querySelector(".eyebox").closest(".password-container");
+  let passwordInput = container.querySelector("input");
+  let eyeVisible = container.querySelector(".eyeVisibility");
+  let eyeHidden = container.querySelector(".eyeNoneVisibility");
 
-  if (container) {
-    let passwordInput =
-      container.querySelector('input[type="password"]') ||
-      container.querySelector('input[type="text"]');
-
-    if (passwordInput) {
-      passwordInput.type =
-        passwordInput.type === "password" ? "text" : "password";
-    }
-  }
-
-  if (eyeNoneVisibility.classList.contains("d-none")) {
-    eyeNoneVisibility.classList.remove("d-none");
-    eyeVisibility.classList.add("d-none");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeVisible.classList.add("d-none");
+    eyeHidden.classList.remove("d-none");
   } else {
-    eyeNoneVisibility.classList.add("d-none");
-    eyeVisibility.classList.remove("d-none");
+    passwordInput.type = "password";
+    eyeVisible.classList.remove("d-none");
+    eyeHidden.classList.add("d-none");
   }
 }
 
@@ -149,8 +141,8 @@ window.addEventListener("DOMContentLoaded", () => {
     updateHeaderButtons(false);
   }
 
-  document.querySelectorAll(".eye, .eyebox").forEach((eyeElement) => {
-    eyeElement.addEventListener("click", togglePasswordVisibility);
+  document.querySelectorAll(".eyebox").forEach((eyebox) => {
+    eyebox.addEventListener("click", togglePasswordVisibility);
   });
 
   let loginForm = document.querySelector(".logIn-container form");
@@ -243,3 +235,7 @@ window.addEventListener("DOMContentLoaded", () => {
     ).innerHTML = `<path d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>`;
   }
 });
+
+function home() {
+  
+}
