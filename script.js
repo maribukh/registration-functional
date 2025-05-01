9df07e8f50e1434676ca3f371b46d2f1843e960f
maribukh <mariebukhaidze@gmail.com>
2025-05-01 10:54:36 +0400

    Removed From Visibility li

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let elements = {
   regButton: document.querySelector("button.brn.brn-second--default"),
 };
 
+function Home() {
+  let homeIcon = document.getElementById("#home");
+  if (homeIcon) {
+    homeIcon.innerHTML = "fdfdf"
+  }
+}
+
 function logIn() {
   let loginContainer = document.querySelector(".logIn-container");
   let registrationContainer = document.querySelector(".registration-container");
@@ -109,17 +116,21 @@ function showWelcome(name) {
 
   updateHeaderButtons(true);
 }
-
 function updateHeaderButtons(isLoggedIn) {
-  let loginBtn = document.querySelector("button#logIn");
-  let regBtn = document.querySelector("button.brn.brn-second--default");
+  let loginBtnLi = document.querySelector("button#logIn")?.closest("li");
+  let regBtnLi = document
+    .querySelector("button.brn.brn-second--default")
+    ?.closest("li");
+  let userWelcomeLi = document.querySelector("#user-welcome")?.closest("li");
 
   if (isLoggedIn) {
-    if (loginBtn) loginBtn.style.display = "none";
-    if (regBtn) regBtn.style.display = "none";
+    if (loginBtnLi) loginBtnLi.style.display = "none";
+    if (regBtnLi) regBtnLi.style.display = "none";
+    if (userWelcomeLi) userWelcomeLi.style.display = "block";
   } else {
-    if (loginBtn) loginBtn.style.display = "block";
-    if (regBtn) regBtn.style.display = "block";
+    if (loginBtnLi) loginBtnLi.style.display = "block";
+    if (regBtnLi) regBtnLi.style.display = "block";
+    if (userWelcomeLi) userWelcomeLi.style.display = "none";
   }
 }
 
@@ -235,4 +246,3 @@ window.addEventListener("DOMContentLoaded", () => {
     ).innerHTML = `<path d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>`;
   }
 });
-
